71afe1dd51f0bfea9ea739e0d9ddb1ec668b59d9
Andres Ruiz <andlopru@gmail.com>
2022-03-02 23:50:34 -0600

    Restructuring

---
+++ app/src/main/java/com/example/animalrand/view/home/HomeFragment.kt
@@ -0,0 +1,57 @@
+package com.example.animalrand.view.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.animalrand.databinding.FragmentHomeBinding
+import com.example.animalrand.di.factory.FactoryViewModel
+import com.example.animalrand.view.adapters.AnimalAdapter
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import dagger.android.support.AndroidSupportInjection
+import dagger.android.support.DaggerFragment
+import javax.inject.Inject
+
+class HomeFragment : DaggerFragment() {
+    @Inject
+    lateinit var viewModelFactory: FactoryViewModel
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+       homeViewModel = ViewModelProvider(this,viewModelFactory)[HomeViewModel::class.java]
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = homeViewModel
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.animalList.apply {
+            adapter = AnimalAdapter()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
